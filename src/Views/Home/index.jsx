2c0b38df47382f7da7bb0bf16c310f9962e302c2
Suryavanshi11444 <sagarmodi2005@gmail.com>
2025-02-11 15:42:30 +0530

    new file

---
+++ src/Views/Home/index.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Home from './Pages/Home'
+import Header  from './Components/Header';
+import { Route,Routes,BrowserRouter } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+     <Route path="/" exact={true} element={<Home/>}/> 
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
